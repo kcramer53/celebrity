20a4a4614c8d618b10cc5814dfde1e32ad20173a
Kirsten Putman and Kira Roberts <kira@>
2019-06-06 14:09:44 -0700

    Add more if else funtionality

--- js/scripts.js
+++ js/scripts.js
@@ -12,19 +12,36 @@ $(document).ready(function() {
     var favoriteMovieTv = $("input:radio[name=movie-tv]:checked").val();
     var favoriteVacation = $("input:radio[name=vacation]:checked").val();
 
-    if (favoriteColor === "green" && favoriteFood === "sushi" && favoriteMovieTv === "notebook" && favoriteVacation === "bali")
-    $('#ryan').show();
-
-    if (favoriteColor === "red" && favoriteFood === "pizza" && favoriteMovieTv === "cheers" && favoriteVacation === "mexico")
-    $('#woody').show();
-
-    if (favoriteColor === "blue" && favoriteFood === "cheeseburger" && favoriteMovieTv === "misery" && favoriteVacation === "switzerland")
-    $('#kathy').show();
-
-    if (favoriteColor === "purple" && favoriteFood === "fried-chicken" && favoriteMovieTv === "sense8" && favoriteVacation === "tahiti") {
-    $('#jamie').show();
+    if (favoriteColor === "green" && favoriteFood === "sushi" && favoriteMovieTv === "notebook" && favoriteVacation === "bali") {
+      $('#ryan').show();
+      $('#woody, #kathy, #jamie, #no-match').hide();
+      // $('#kathy').hide();
+      // $('#jamie').hide();
+      // $('#no-match').hide();
+    } else if (favoriteColor === "red" && favoriteFood === "pizza" && favoriteMovieTv === "cheers" && favoriteVacation === "mexico") {
+      $('#woody').show();
+      $('#ryan').hide();
+      $('#kathy').hide();
+      $('#jamie').hide();
+      $('#no-match').hide();
+    } else if (favoriteColor === "blue" && favoriteFood === "cheeseburger" && favoriteMovieTv === "misery" && favoriteVacation === "switzerland") {
+      $('#kathy').show();
+      $('#ryan').hide();
+      $('#woody').hide();
+      $('#jamie').hide();
+      $('#no-match').hide();
+    } else if (favoriteColor === "purple" && favoriteFood === "fried-chicken" && favoriteMovieTv === "sense8" && favoriteVacation === "tahiti") {
+      $('#jamie').show();
+      $('#ryan').hide();
+      $('#woody').hide();
+      $('#kathy').hide();
+      $('#no-match').hide();
     } else {
       $('#no-match').show();
+      $('#jamie').hide();
+      $('#ryan').hide();
+      $('#woody').hide();
+      $('#kathy').hide();
     }
 
   event.preventDefault();
